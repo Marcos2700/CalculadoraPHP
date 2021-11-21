d9fcf0581e54763c308eb68c15a780b933e12384
Marcos2700 <marcosaer2772@gmail.com>
2021-11-21 13:29:08 -0500

    1.0

--- app/CalculadoraTest.php
+++ app/CalculadoraTest.php
@@ -4,6 +4,25 @@ use \PHPUnit\Framework\TestCase;
 class CalculadoraTest extends TestCase{
     public function testSumar(){
         $calculadora = new Calculadora();
-        $this->assertEquals(6,$calculadora->sumar(3,3));
+        //$this->assertEquals(10,$calculadora->sumar(8,2));
+        $this->assertSame(10,$calculadora->sumar(8,2));
+    }
+    public function testRestar(){
+        $calculadora = new Calculadora();
+        $this->assertEquals(6,$calculadora->restar(8,2));
+    }
+    public function testMultiplicar(){
+        $calculadora = new Calculadora();
+        $this->assertEquals(16,$calculadora->multiplicar(8,2));
+    }
+    public function testDividir(){
+        $calculadora = new Calculadora();
+        $this->assertEqualsWithDelta(0.33,$calculadora->dividir(1,3), 0.003);
+    }
+    public function testGenerarArreglo(){
+        $calculadora = new Calculadora();
+        //$this->assertContains(5, $calculadora->generarArreglo());
+        //$this->assertCount(6, $calculadora->generarArreglo());
+        $this->assertNotEmpty($calculadora->generarArreglo());
     }
 }
