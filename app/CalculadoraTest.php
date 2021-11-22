1c3eea41fff89b9738d38b2c11cc2b77c9d1f8c2
Marcos2700 <marcosaer2772@gmail.com>
2021-11-22 09:21:41 -0500

    1.0

--- app/CalculadoraTest.php
+++ app/CalculadoraTest.php
@@ -2,22 +2,80 @@
 require_once(__DIR__.'/Calculadora.php');
 use \PHPUnit\Framework\TestCase;
 class CalculadoraTest extends TestCase{
-    public function testSumar(){
+
+    public function sumarProveedor()
+    {
+       return [
+           'Caso 1' => [-1, -1, -2],
+           'Caso 2' => [0, 0, 0],
+           'Caso 3' => [0, -1, -1],
+           'Caso 4' => [-1, 0, -1]
+       ];
+    }
+    public function restarProveedor(){
+        return[
+            'Caso 1' => [-1, -1, 0],
+            'Caso 2' => [0, 0, 0],
+            'Caso 3' => [0, -1, 1],
+            'Caso 4' => [-1, 0, -1]
+        ];
+    }
+    public function multiplicarProveedor(){
+        return[
+            'Caso 1' => [-1, -1, 1],
+            'Caso 2' => [0, 0, 0],
+            'Caso 3' => [0, -1, 0],
+            'Caso 4' => [-1, 0, 0]
+        ];
+    }
+    public function dividirProveedor(){
+        return[
+            'Caso 1' => [-1, -1, 1, 0],
+            'Caso 2' => [0, 0, "Exception", ""],
+            'Caso 3' => [0, -1, 0, 0],
+            'Caso 4' => [-1, 0, "Exception", ""],
+            'Caso 5' => [1, 3, 0.33, 0.01]
+        ];
+    }
+
+    /**
+    * @dataProvider sumarProveedor
+    */
+    public function testSumar($numero1, $numero2, $resultado_esperado){
         $calculadora = new Calculadora();
         //$this->assertEquals(10,$calculadora->sumar(8,2));
-        $this->assertSame(10,$calculadora->sumar(8,2));
+        //$this->assertSame(10,$calculadora->sumar(8,2));
+        $this->assertEquals($resultado_esperado,$calculadora->sumar($numero1,$numero2));
     }
-    public function testRestar(){
+    /**
+    * @dataProvider restarProveedor
+    */
+    public function testRestar($numero1, $numero2, $resultado_esperado){
         $calculadora = new Calculadora();
-        $this->assertEquals(6,$calculadora->restar(8,2));
+        //$this->assertEquals(6,$calculadora->restar(8,2));
+        $this->assertEquals($resultado_esperado,$calculadora->restar($numero1,$numero2));
     }
-    public function testMultiplicar(){
+    /**
+    * @dataProvider multiplicarProveedor
+    */
+    public function testMultiplicar($numero1, $numero2, $resultado_esperado){
         $calculadora = new Calculadora();
-        $this->assertEquals(16,$calculadora->multiplicar(8,2));
+        //$this->assertEquals(16,$calculadora->multiplicar(8,2));
+        $this->assertEquals($resultado_esperado,$calculadora->multiplicar($numero1,$numero2));
     }
+    /**
+    * @dataProvider dividirProveedor
+    */
     public function testDividir(){
-        $calculadora = new Calculadora();
-        $this->assertEqualsWithDelta(0.33,$calculadora->dividir(1,3), 0.003);
+        $calculadora = new Calculadora($numero1, $numero2, $resultado_esperado, $delta);
+        if($numero2 != 0){
+            $this->assertEqualsWithDelta($resultado_esperado, $calculadora->dividir($numero1, $numero2), $delta);
+        }
+        else{
+            $this->expectException('Exception');
+            $calculadora->dividir($numero1, $numero2);
+        }
+        //$this->assertEqualsWithDelta(0.33,$calculadora->dividir(1,3), 0.003));
     }
     public function testGenerarArreglo(){
         $calculadora = new Calculadora();
